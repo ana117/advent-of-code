a24b4b4e4cbb307af705fef9e6ef3bf82294c1fe
ana117 <63540951+ana117@users.noreply.github.com>
2025-12-03 19:28:15 +0700

    remove unnecessary whitespaces

--- generate.py
+++ generate.py
@@ -23,33 +23,33 @@ if __name__ == "__main__":
 \"\"\"
 Write the question summary here
 \"\"\"
-              
+
 test_data_a = \"\"\"
 
 \"\"\".strip()
 test_data_b = test_data_a
 test_answer_a = 0
 test_answer_b = test_answer_a
-              
+
 
 def solve_a(data):
   answer = 0
   return answer
-              
+
 def test_a():
   answer = solve_a(test_data_a)
   assert answer == test_answer_a, f"test_a failed: expected {test_answer_a} but got {answer}"
 
-               
+
 def solve_b(data):
   answer = 0
   return answer
-              
+ 
 def test_b():
   answer = solve_b(test_data_b)
   assert answer == test_answer_b, f"test_b failed: expected {test_answer_b} but got {answer}"
 
-              
+
 if __name__ == "__main__":
   test_a()
   test_b()
